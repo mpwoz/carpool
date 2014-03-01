347bc8f0374e75e38ebd94d664b757945b7381d6
mpwoz <martin.woz@gmail.com>
2014-03-01 17:11:09 -0600

    Bug

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,7 @@ var rideData = require('../models/rides');
 // If successful, return the id of the new row
 exports.newRide = function (req, res) {
   rideData.insert(req.body, function(result) {
-    if result.affectedRows === 1 {
+    if (result.affectedRows === 1) {
       res.send(200, {'id': result.insertId});
     } else {
       res.send(500, "Insert unsuccessful, check server logs.");
