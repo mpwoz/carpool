51079c72d7e63cb032892f0b2b49adc0ed485b8f
gli3355 <gary@gary-VirtualBox.(none)>
2014-04-06 16:14:48 -0500

    add feedback test(not fully working)

--- test/server/server-spec.js
+++ test/server/server-spec.js
@@ -13,3 +13,37 @@ describe("server", function() {
     });
   });
 });
+
+describe("server", function() {
+  it("should fetch a user's feedback", function(done) {
+    request.get('http://localhost:8000/api/feedback/wozniew1@illinois.edu', function(error, response, body) {
+      body = JSON.parse(body);
+      //console.log(body);
+      expect(body[0].from).toEqual('gli24@illinois.edu');
+      expect(body[0].score).toEqual(5);
+      expect(body[1].from).toEqual('bajekal1@illinois.edu');
+      expect(body[1].score).toEqual(3);
+      done();
+    });
+  });
+});
+
+/*describe("server", function() {
+  it("should add a user's feedback", function(done) {
+  	var feedbackJSON = {"from": "wyang15@illinois.edu",
+  											"to": "wozniew1@illinois.edu",
+  											"ride_id": 1,
+  											"comment": "good ride",
+  											"score": 1
+  											};
+    request.post('http://localhost:8000/api/feedback/',{form:{"from": "wyang15@illinois.edu",
+  											"to": "wozniew1@illinois.edu",
+  											"ride_id": 1,
+  											"comment": "good ride",
+  											"score": 1
+  											}}, function(error, response, body) {
+
+      done();
+    });
+  });
+});*/
