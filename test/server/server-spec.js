55ebabe1af731f1301d5635e9492c64e706347f5
wyang15 <wypluy@gmail.com>
2014-04-06 14:30:55 -0700

    Added a test for API call to fetch rides by user with kshu2

--- test/server/server-spec.js
+++ test/server/server-spec.js
@@ -13,3 +13,14 @@ describe("server", function() {
     });
   });
 });
+
+describe("server", function() {
+	it("should get a list of user rides", function(done) {
+		request.get('http://localhost:8000/api/userrides/wozniew1', function(error, response, body) {
+			body = JSON.parse(body);
+			expect(body[0].id).toEqual(1);
+			expect(body[0].riders.length).toEqual(3);
+			done();
+		})
+	});
+});
