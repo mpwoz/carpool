c006fad5b7f913487b8a08ef421b81e306a0ce28
mpwoz <martin.woz@gmail.com>
2014-04-06 15:16:09 -0500

    e2e test for rider API

--- test/server/server-spec.js
+++ test/server/server-spec.js
@@ -1,27 +1,15 @@
 'use strict'
 
-var api = require('../../routes/api'),
-    request = require('request');
-
-/* jasmine specs for server go here */
+var request = require('request');
 
 describe("server", function() {
   it("should fetch a ride's details", function(done) {
-    request('http://localhost:8000/api/rides/1', function(err, res, body) {
-      console.log(res);
-      console.log(body);
+    request.get('http://localhost:8000/api/rides/1', function(error, response, body) {
+      body = JSON.parse(body);
+      //console.log(body);
+      expect(body.id).toEqual(1);
+      expect(body.riders.length).toEqual(3);
       done();
     });
-  //  var mockReq = {
-  //    params: { id: 1 }
-  //  };
-  //  var mockRes = {
-  //    json: function(ride) {
-  //      console.log(ride);
-  //      expect(ride).toBeDefined();
-  //      done();
-  //    }
-  //  };
-  //  api.getRide(mockReq, mockRes);
   });
 });
