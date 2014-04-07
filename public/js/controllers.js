4e98495ae44fb641667ebb0ca21a25c864a90418
leestephen <yoo929@gmail.com>
2014-04-07 15:46:15 -0500

    Refactored Pattern Matching on New Ride

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -10,12 +10,22 @@ angular.module('myApp.controllers', []).
   }).
   controller('NewRideCtrl', function ($scope, RideFactory) {
     $scope.createRide = function() {
+      // Variables from entry fields
       var email = $scope.email;
       var startLocation = $scope.startLocation;
       var endLocation = $scope.endLocation;
       var seats = $scope.seats;
       var seatPrice = $scope.seatPrice;
       var departureTime = $scope.departureTime;
+
+      // Pattern matching for correct values
+      var isValidEmail = ! $scope.newride_form.email.$error.pattern;
+      var isValidStartLoc = ! $scope.newride_form.startLocation.$error.pattern;
+      var isValidEndLoc = ! $scope.newride_form.endLocation.$error.pattern;
+      var isValidLocations = isValidStartLoc && isValidEndLoc;
+      var seatsAvailable = seats > 0;
+      var priceIsValid = seatPrice >= 0;
+      
       var ride = {
         'email': email,
         'startLocation': startLocation,
@@ -24,11 +34,11 @@ angular.module('myApp.controllers', []).
         'seatPrice': seatPrice,
         'departureTime': departureTime
       }
-      if (! $scope.newride_form.email.$error.pattern && ! $scope.newride_form.startLocation.$error.pattern &&
-          ! $scope.newride_form.endLocation.$error.pattern && seats>0 && seatPrice>=0) {
+
+      if (isValidEmail && isValidLocations && seatsAvailable && priceIsValid) {
         RideFactory.newRide(ride)
           .success(function(data, status, headers, config) {
-            $scope.success = "Ride Created!";
+            $scope.success = "Ride Created.";
           }).error(function(data, status, headers, config) {
             $scope.fail = "Failed to create ride.";
           });
