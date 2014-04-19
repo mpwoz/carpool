92ddd68d56a947b1ae004274562cabc31f9c24e1
Shil <shilb10@gmail.com>
2014-04-19 14:25:04 -0500

    Edited working datepicker

--- public/js/filters.js
+++ public/js/filters.js
@@ -14,15 +14,3 @@ angular.module('myApp.filters', [])
       return matchedRides.length > 0 ? matchedRides : rides;
     }
   });
-  // filter('date', function() {
-  //   return function (rides, depart) {
-  //     var matchedRides = [];
-  //     console.log(rides);
-  //     angular.forEach(rides, function(ride) {
-  //       var date = new Date(ride.departureTime);
-  //       // if (depart && date.getDay() === depart.getDay() && date.getMonth() === depart.getMonth()) {
-  //       //   matchedRides.push(ride);
-  //       // }
-  //     });
-  //   }
-  // });
