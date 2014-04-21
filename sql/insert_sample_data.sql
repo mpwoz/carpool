c38072be42fa505b729b97c8234940bb852cc064
mpwoz <martin.woz@gmail.com>
2014-04-21 17:06:51 -0500

    Add tokens for prepopulated data and decrement available seats on ride detail page.

--- sql/insert_sample_data.sql
+++ sql/insert_sample_data.sql
@@ -1,11 +1,11 @@
-INSERT INTO `carpool_db`.`ride` (`email`, `confirmed`, `startLocation`, `endLocation`, `seats`, `seatPrice`, `departureTime`) VALUES
-  ('wozniew1@illinois.edu', 1, 'University of Illinois at Urbana-Champaign', 'O\'Hare International Airport', 4, 20, "2014-03-12T20:50:48.000Z"),
-  ('gli24@illinois.edu', 0, 'University of Illinois at Urbana-Champaign', 'O\'Hare International Airport', 4, 20, "2014-03-12T20:50:48.000Z"),
-  ('kshu2@illinois.edu', 1, 'Naperville', 'University of Illinois at Urbana-Champaign', 4, 25, "2014-03-15T20:50:48.000Z"),
-  ('bajekal1@illinois.edu', 0, 'Naperville', 'University of Illinois at Urbana-Champaign', 4, 25, "2014-03-15T20:50:48.000Z"),
-  ('sbhttch2@illinois.edu', 1, 'University of Illinois at Urbana-Champaign', 'O\'Hare International Airport', 3, 30, "2014-03-10T20:50:48.000Z"),
-  ('slee117@illinois.edu', 0, 'University of Illinois at Urbana-Champaign', 'O\'Hare International Airport', 3, 30, "2014-03-10T20:50:48.000Z"),
-  ('wyang15@illinois.edu', 1, 'O\'Hare International Airport', 'University of Illinois at Urbana-Champaign', 5, 40, "2014-03-10T20:50:48.000Z");
+INSERT INTO `carpool_db`.`ride` (`id`, `email`, `confirmed`, `startLocation`, `endLocation`, `seats`, `seatPrice`, `departureTime`) VALUES
+  (1, 'wozniew1@illinois.edu', 1, 'University of Illinois at Urbana-Champaign', 'O\'Hare International Airport', 4, 20, "2014-03-12T20:50:48.000Z"),
+  (2, 'gli24@illinois.edu', 0, 'University of Illinois at Urbana-Champaign', 'O\'Hare International Airport', 4, 20, "2014-03-12T20:50:48.000Z"),
+  (3, 'kshu2@illinois.edu', 1, 'Naperville', 'University of Illinois at Urbana-Champaign', 4, 25, "2014-03-15T20:50:48.000Z"),
+  (4, 'bajekal1@illinois.edu', 0, 'Naperville', 'University of Illinois at Urbana-Champaign', 4, 25, "2014-03-15T20:50:48.000Z"),
+  (5, 'sbhttch2@illinois.edu', 1, 'University of Illinois at Urbana-Champaign', 'O\'Hare International Airport', 3, 30, "2014-03-10T20:50:48.000Z"),
+  (6, 'slee117@illinois.edu', 0, 'University of Illinois at Urbana-Champaign', 'O\'Hare International Airport', 3, 30, "2014-03-10T20:50:48.000Z"),
+  (7, 'wyang15@illinois.edu', 1, 'O\'Hare International Airport', 'University of Illinois at Urbana-Champaign', 5, 40, "2014-03-10T20:50:48.000Z");
 
 
 
@@ -20,4 +20,10 @@ INSERT INTO `carpool_db`.`feedback` (`from`, `to`, `ride_id`, `comment`, `score`
   ('bajekal1@illinois.edu', 'wozniew1@illinois.edu', 1, 'hello!', 4);
 
 INSERT INTO `carpool_db`.`token` (`ride_id`, `token`, `created`) VALUES
-  (1, 'abcdefg', '2014-03-15T20:50:48.000Z');
+  (1, 'ride1', '2014-03-15T20:50:48.000Z'),
+  (2, 'ride2', '2014-03-15T20:50:48.000Z'),
+  (3, 'ride3', '2014-03-15T20:50:48.000Z'),
+  (4, 'ride4', '2014-03-15T20:50:48.000Z'),
+  (5, 'ride5', '2014-03-15T20:50:48.000Z'),
+  (6, 'ride6', '2014-03-15T20:50:48.000Z'),
+  (7, 'ride7', '2014-03-15T20:50:48.000Z');
