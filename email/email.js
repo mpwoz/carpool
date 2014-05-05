dfa7636d95d717ec71c988a4eb8db464d4900a35
Martin Wozniewicz <martin.woz@gmail.com>
2014-05-04 23:00:05 -0500

    Refactor email module to eliminate code duplication

--- email/email.js
+++ email/email.js
@@ -1,55 +1,62 @@
+/*
+ * This class handles all the email sending functionality for our application.
+ * Each specific email to send has its own function which populates it with
+ * the correct text.
+ */
+
 var path           = require('path')
-  , templatesDir   = path.resolve(__dirname, '.', 'templates')
   , nodemailer     = require('nodemailer');
 
-module.exports.sendConfirmationEmail = function(address, confirmURL) {
-	    // ## Send a single email
-      console.log('Send confirmation to ' + address);
-
-	    // Prepare nodemailer transport object
-	    var transport = nodemailer.createTransport("SMTP", {
+/*
+ * Connection details for the email account
+ */
+var emailConfig = {
 	      service: "Gmail",
 	      auth: {
           user: "carpoolingtest1@gmail.com",
           pass: "asdfasdf1231234"
 	      }
-	    });
+	    };
 
-	    // Send a single email
-		transport.sendMail({
-		  from: 'Carpooling <carpoolingtest1@gmail.com>',
-		  to: address,
-		  subject: 'Confirm your carpool.',
-		  html: '<a href="'+ confirmURL+'"> Click here </a>',
-		  text: "Click this link: "+ confirmURL
-		}, function(err, responseStatus) {
-		  if (err) {
-		    console.log(err);
-		  } else {
-		    console.log(responseStatus.message);
-		  }
-		});
+/*
+ * Send the ride creation confirmation email
+ */
+module.exports.sendConfirmationEmail = function(address, confirmURL) {
+  sendEmail(address, {
+    subject: 'Confirm your carpool.',
+    html: '<a href="'+ confirmURL+'"> Click here </a>',
+    text: "Click this link: "+ confirmURL
+  });
 }
 
+/*
+ * Send the deletion confirmation email
+ */
 module.exports.sendDeletionEmail = function(address, confirmURL) {
-	    var transport = nodemailer.createTransport("SMTP", {
-	      service: "Gmail",
-	      auth: {
-          user: "carpoolingtest1@gmail.com",
-          pass: "asdfasdf1231234"
-	      }
-	    });
-	    transport.sendMail({
-		  from: 'Carpooling <carpoolingtest1@gmail.com>',
-		  to: address,
-		  subject: 'Confirm your request to delete ride.',
-		  html: '<a href="'+ confirmURL+'"> Click here </a>',
-		  text: "Click this link: "+ confirmURL
-		}, function(err, responseStatus) {
-		  if (err) {
-		    console.log(err);
-		  } else {
-		    console.log(responseStatus.message);
-		  }
-		});		
+  sendEmail(address, {
+    subject: 'Carpool - Confirm your request to delete ride.',
+    html: '<a href="'+ confirmURL+'"> Click here to delete your ride </a>',
+    text: "Visit this page to delete your ride: "+ confirmURL
+  });
+}
+
+
+/*
+ * Private function for sending an email from our account.
+ */
+var sendEmail = function(address, options) {
+  var transport = nodemailer.createTransport("SMTP", emailConfig);
+  transport.sendMail({
+      from: 'Carpooling <carpoolingtest1@gmail.com>',
+      to: address,
+      subject: options.subject || "Carpooling - error email",
+      html: options.html || "<p>You should not receive this message, please contact carpool developers</p>",
+      text: options.text || "You should not receive this message, please contact carpool developers",
+  }, function(err, responseStatus) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(responseStatus.message);
+    }
+  });		
 }
