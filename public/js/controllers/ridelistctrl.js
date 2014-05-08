29a68647a4eaf09bcfacf2ead8c5177ab106ceef
mpwoz <martin.woz@gmail.com>
2014-05-08 16:19:16 -0500

    Datepicker automatically updates instead of having to push a button

--- public/js/controllers/ridelistctrl.js
+++ public/js/controllers/ridelistctrl.js
@@ -30,4 +30,68 @@ angular.module('myApp.controllers')
       });
     };
 
+    // Filter dates based on what's currently selected in the datepicker
+    $scope.dateFilter = function (row) {
+      // No date selected, don't filter anything
+      if ($scope.dt === null) {
+        return true;
+      }
+      var rowTime = new Date(row.departureTime);
+      var pickedTime = new Date($scope.dt);
+      return (pickedTime.getDate() === rowTime.getDate() 
+              && pickedTime.getMonth() === rowTime.getMonth());
+    };
+
+    // Old DatePicker stuff
+    $scope.today = function() {
+      $scope.dt = new Date();
+    };
+
+    $scope.showWeeks = true;
+    $scope.toggleWeeks = function () {
+      $scope.showWeeks = ! $scope.showWeeks;
+    };
+
+    $scope.clear = function () {
+      $scope.dt = null;
+    };
+    $scope.clear();
+
+    $scope.chooseDate = function() {
+      var date1 = new Date($scope.dt);
+      var matchedRides = [];
+      for(var i=0; i<$scope.globalRides.length; i++) {
+        var date = new Date($scope.globalRides[i].departureTime);
+        if (date1.getDate() === date.getDate() && date1.getMonth() === date.getMonth()) {
+          matchedRides.push($scope.globalRides[i]);
+        }
+      }
+      $scope.rides = matchedRides;
+      $scope.$apply;
+    }
+
+    // Disable weekend selection
+    $scope.disabled = function(date, mode) {
+      return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+    };
+
+    $scope.toggleMin = function() {
+      $scope.minDate = ( $scope.minDate ) ? null : new Date();
+    };
+    $scope.toggleMin();
+
+    $scope.open = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+
+      $scope.opened = true;
+    };
+
+    $scope.dateOptions = {
+      'year-format': "'yy'",
+      'starting-day': 1
+    };
+
+    $scope.formats = ['MMM dd, yyyy', 'dd-MMMM-yyyy', 'yyyy/MM/dd', 'shortDate'];
+    $scope.format = $scope.formats[0];
   });
