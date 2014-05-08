e5c3d7c196d2c0588e9e1f5f6b95419d2bce6fe3
mpwoz <martin.woz@gmail.com>
2014-05-08 16:35:00 -0500

    Cleaned up ride list page code and controller

--- public/js/controllers/ridelistctrl.js
+++ public/js/controllers/ridelistctrl.js
@@ -42,54 +42,23 @@ angular.module('myApp.controllers')
               && pickedTime.getMonth() === rowTime.getMonth());
     };
 
-    // Old DatePicker stuff
-    $scope.today = function() {
-      $scope.dt = new Date();
-    };
-
-    $scope.showWeeks = true;
-    $scope.toggleWeeks = function () {
-      $scope.showWeeks = ! $scope.showWeeks;
-    };
-
-    $scope.clear = function () {
-      $scope.dt = null;
-    };
-    $scope.clear();
-
-    $scope.chooseDate = function() {
-      var date1 = new Date($scope.dt);
-      var matchedRides = [];
-      for(var i=0; i<$scope.globalRides.length; i++) {
-        var date = new Date($scope.globalRides[i].departureTime);
-        if (date1.getDate() === date.getDate() && date1.getMonth() === date.getMonth()) {
-          matchedRides.push($scope.globalRides[i]);
-        }
-      }
-      $scope.rides = matchedRides;
-      $scope.$apply;
-    }
+    $scope.dt = null; // Clear the date to begin with, no filter
 
     // Disable weekend selection
     $scope.disabled = function(date, mode) {
       return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
     };
 
-    $scope.toggleMin = function() {
-      $scope.minDate = ( $scope.minDate ) ? null : new Date();
-    };
-    $scope.toggleMin();
-
     $scope.open = function($event) {
       $event.preventDefault();
       $event.stopPropagation();
-
       $scope.opened = true;
     };
 
     $scope.dateOptions = {
       'year-format': "'yy'",
-      'starting-day': 1
+      'starting-day': 1,
+      'show-weeks': false
     };
 
     $scope.formats = ['MMM dd, yyyy', 'dd-MMMM-yyyy', 'yyyy/MM/dd', 'shortDate'];
